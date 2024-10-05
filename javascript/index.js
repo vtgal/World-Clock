3c77f1aba3ff9a005f8f72a45a807df09dbcb775
vtgal <vtskyle101@gmail.com>
2024-10-04 21:18:18 -0500

    Added current location.

--- javascript/index.js
+++ javascript/index.js
@@ -26,7 +26,11 @@ parisTimeElement.innerHTML = parisTime.format("h:mm:ss [<small>]A[</small>]");
 }
 
 function updateCity(event) {
-    let cityTimeZone = event.target.value; 
+    let cityTimeZone = event.target.value;
+    if (cityTimeZone === "current"){
+    cityTimeZone = moment.tz.guess();
+} 
+
     let cityName = cityTimeZone.replace("_"," ").split("/")[1];
     let cityTime = moment().tz(cityTimeZone);
     let citiesElement = document.querySelector("#cities");
